2109166bb9fb53c07dfb45e776c16fc9dc0d15f1
rjdsa-iscte <rjdsa@iscte-iul.pt>
2020-11-03 16:02:14 +0000

    Esboço_Gui_V0.1

--- ES-LETI-1Sem-2020-Grupo9/src/main/java/GUI/GUI.java
+++ ES-LETI-1Sem-2020-Grupo9/src/main/java/GUI/GUI.java
@@ -6,6 +6,25 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Panel;
+import java.awt.Button;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.Box;
+import javax.swing.JTable;
+import javax.swing.JLabel;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import javax.swing.JFileChooser;
+import javax.swing.JToolBar;
+import javax.swing.JSplitPane;
+import java.awt.FlowLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JTabbedPane;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.BevelBorder;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JMenu;
 
 public class GUI extends JFrame {
 
@@ -32,7 +51,19 @@ public class GUI extends JFrame {
 	 */
 	public GUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1200, 600);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("File");
+		menuBar.add(mnNewMenu);
+		
+		JMenu mnNewMenu_2 = new JMenu("View File");
+		mnNewMenu.add(mnNewMenu_2);
+		
+		JMenu mnNewMenu_1 = new JMenu("Load File");
+		mnNewMenu.add(mnNewMenu_1);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
