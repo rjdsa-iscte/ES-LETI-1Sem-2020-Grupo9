4ad59e46d91bed6e098549fe977ea823aa509a3b
rjdsa-iscte <rjdsa@iscte-iul.pt>
2020-12-08 19:18:26 +0000

    Junit 5 library

---
+++ ES-LETI-1Sem-2020-Grupo9/src/test/java/ES_LETI_1Sem_2020_Grupo9/test.java
@@ -0,0 +1,14 @@
+package ES_LETI_1Sem_2020_Grupo9;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
